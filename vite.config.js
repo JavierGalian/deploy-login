8e1170a3ab05f79c0b753fdea35bd52d85f77249
Javier Rodrigo Galian <javier.galian.1997@gmail.com>
2023-05-07 09:50:36 -0300

    update vite.config.js base: https://JavierGalian.github.io/deploy-login/

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: 'https://github.com/JavierGalian/deploy-login/',
+  https: "https://JavierGalian.github.io/deploy-login/",
 })
